4e56601dfd991b9477bdb2a17d55f412955be3e9
Casey MacLeod <caseymacleod22@gmail.com>
2023-12-24 14:34:32 -0500

    first commit, basic react setup and bootstrap

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import './bootstrap-css/bootstrap.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="row">
+            <p>What's up cucks</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
