3ce4cfd08b3896e8aed62eabc9b104ded4e6eaeb
Casey MacLeod <caseymacleod22@gmail.com>
2023-12-25 00:22:17 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import './App.css';
 import './bootstrap-css/bootstrap.css';
+import { useEffect, useState } from 'react'
+import TeamName from './components/TeamName';
+import Starters from './components/Starters';
+
 
 function App() {
+
+  const [appData, setAppData] = useState([])
+  // we also need to initialize component state with useState
+
+  async function getAppData() {
+    let test = 'oh yea im, testing my guy';
+  }
+
+  useEffect(() => {
+    getAppData()
+  }, [])
+
   return (
     <div className="App">
       <div className="container">
-        <div className="row">
-            <p>What's up cucks</p>
-        </div>
+        <TeamName />
+        <Starters />
       </div>
     </div>
   );
---
+++ src/components/TeamName.js
@@ -0,0 +1,7 @@
+const TeamName = (props) => {
+    return (
+        <h2 class="col-12">Girtho's Girth</h2>
+    )
+}
+
+export default TeamName
